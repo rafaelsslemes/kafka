5b91a4942355f519dc15e6fc45fdc1947ea61a2a
Rafael Lemes <rafael.sslemes@gmail.com>
2022-12-11 09:05:30 -0300

    Callbacks on sendMessage

--- str-producer/src/main/java/com/study/strproducer/service/StringProducerService.java
+++ str-producer/src/main/java/com/study/strproducer/service/StringProducerService.java
@@ -4,14 +4,27 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class StringProducerService {
     
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message){
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message)
+        .thenAccept(success -> {
+            log.info("Message sent {}", message);
+            log.info("Partition: {}, Offset: {}", 
+                        success.getRecordMetadata().partition(), 
+                        success.getRecordMetadata().offset());})
+        .exceptionally(this::onError);
+    }
+
+    private Void onError(Throwable error){
+        log.error("Error sending message");
+        return null;
     }
 }
