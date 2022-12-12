60a0030cfbbeb2dc6f8e308743a7fb333867ea1f
Rafael Lemes <rafael.sslemes@gmail.com>
2022-12-12 14:05:35 -0300

    Testing group concurrency

--- str-consumer/src/main/java/com/study/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/study/strconsumer/listeners/StrConsumerListener.java
@@ -9,8 +9,21 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class StrConsumerListener {
 
+    // Listeners from same group compete to partitions of topic
+    // A single Listener in a group listens all partitions of topic
+
     @KafkaListener(groupId = "string-group", topics = "str-topic", containerFactory = "strContainerFactory")
     public void listen(String message){
-        log.info("Message received: {}", message);
+        log.info("PROCCESS :: Message received: {}", message);
+    }
+
+    @KafkaListener(groupId = "string-group", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void auditLogs(String message){
+        log.info("LOG ::: Message received: {}", message);
+    }
+
+    @KafkaListener(groupId = "other-string-group", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void paymentProccess(String message){
+        log.info("PAYMENT ::: Message received: {}", message);
     }
 }
