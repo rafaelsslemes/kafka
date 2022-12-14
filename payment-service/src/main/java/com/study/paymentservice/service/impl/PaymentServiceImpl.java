470d4b1dc83b4f33d1d0c594db785b06452c70ba
Rafael Lemes <rafael.sslemes@gmail.com>
2022-12-14 20:27:12 -0300

    removed unused

--- payment-service/src/main/java/com/study/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/study/paymentservice/service/impl/PaymentServiceImpl.java
@@ -1,14 +1,10 @@
 package com.study.paymentservice.service.impl;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.study.paymentservice.model.Payment;
 import com.study.paymentservice.service.PaymentService;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
