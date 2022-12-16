0540183e0505552d7e5f603f9a85668243389a0f
Rafael Lemes <rafael.sslemes@gmail.com>
2022-12-16 07:01:02 -0300

    Folder location fix

--- payment-service/src/main/java/com/study/paymentservice/resource/impl/PaymentResourceImpl.java
+++ payment-service/src/main/java/com/study/paymentservice/resource/impl/PaymentResourceImpl.java
@@ -1,6 +1,9 @@
-package impl;
+package com.study.paymentservice.resource.impl;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +21,10 @@ public class PaymentResourceImpl implements PaymentResource {
     private final PaymentService paymentService;
 
     @Override
-    public ResponseEntity<Payment> payment(Payment payment) {
+    @PostMapping
+    public ResponseEntity<Payment> payment(@RequestBody Payment payment) {
 
         paymentService.sendPayment(payment);
-        
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
     
