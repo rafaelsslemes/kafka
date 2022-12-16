d3413ce5dc85dd0bae0b88768ec02509711e41a2
Rafael Lemes <rafael.sslemes@gmail.com>
2022-12-16 07:01:33 -0300

    Using KafkaTemplate to send json object

--- payment-service/src/main/java/com/study/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/study/paymentservice/service/impl/PaymentServiceImpl.java
@@ -1,20 +1,33 @@
 package com.study.paymentservice.service.impl;
 
+import java.io.Serializable;
+
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.study.paymentservice.model.Payment;
 import com.study.paymentservice.service.PaymentService;
 
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService{
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
     @Override
+    @SneakyThrows // Dismiss throws declaration
     public void sendPayment(Payment payment) {
         log.info("PAYMENT SERVICE ::: Payment received {}", payment);
+        Thread.sleep(1000);
        
+        log.info("Sending Payment");
+         
+        kafkaTemplate.send("payment-topic", payment);
     }
     
 }
